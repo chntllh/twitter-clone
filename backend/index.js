1359f367ba2307529eea4c9a06f612ee9da97a40
Siddhartha Singh <siddhartha.joy@gmail.com>
2024-09-23 00:30:36 +0530

    Create error handler

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,23 @@ dotenv.config({ path: '../.env' });
 
 const app = express();
 
+app.use(express.json())
+
 mongoose
-   .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log(err));
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
+});
+
+app.use((error, req, res, next) => {
+  const statusCode = error.statusCode || 500;
+  const message = error.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });

b76a9a648e12dbacea3df807bc4600fe34954906
Siddhartha Singh <siddhartha.joy@gmail.com>
2024-11-24 14:11:45 +0530

    Update user controller and route

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,7 +1,14 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/user.model";
-import { isValidObjectId } from "mongoose";
+import { isValidObjectId, ObjectId } from "mongoose";
 import { errorHandler } from "../middleware/errorHandler";
+import { compare, hash } from "bcryptjs";
+
+export interface CustomRequest extends Request {
+  user?: {
+    id: ObjectId;
+  };
+}
 
 interface FormattedUser {
   userId: string;
@@ -58,6 +65,107 @@ export const getUser = async (
     const formattedUser: FormattedUser = formatUser(user);
 
     res.status(200).json(formattedUser);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateUser = async (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.user) {
+    return next(errorHandler(404, "Not valid user"));
+  }
+
+  try {
+    const userId: ObjectId = req.user.id;
+    const {
+      username,
+      email,
+      displayName,
+      bio,
+      avatarUrl,
+      password,
+      newPassword,
+    } = req.body;
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return next(errorHandler(404, "User not found"));
+    }
+
+    if (username) {
+      const usernameExists = await User.exists({
+        username,
+        _id: { $ne: userId },
+      });
+      if (usernameExists) {
+        return next(errorHandler(400, "Username already exists"));
+      }
+      if (username.length < 3 || username.length > 16) {
+        return next(
+          errorHandler(400, "Username must be between 3 and 16 characters.")
+        );
+      }
+      if (username.includes(" ")) {
+        return next(errorHandler(400, "Username cannot contain spaces"));
+      }
+      const usernameRegex = /^[a-zA-Z0-9]+$/;
+      if (!usernameRegex.test(username)) {
+        return next(errorHandler(400, "Username can only be alphanumeric"));
+      }
+      user.username = username;
+    }
+
+    if (email) {
+      const emailExist = await User.exists({ email, _id: { $ne: userId } });
+      if (emailExist) {
+        return next(errorHandler(400, "Email already exists"));
+      }
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(email)) {
+        return next(errorHandler(400, "Invalid email format"));
+      }
+      user.email = email;
+    }
+
+    if (displayName) {
+      user.displayName = displayName;
+    }
+
+    if (bio !== undefined) {
+      user.bio = bio;
+    }
+
+    if (avatarUrl) {
+      user.avatarUrl = avatarUrl;
+    }
+
+    if (password && newPassword) {
+      if (await compare(newPassword, user.passwordHash)) {
+        return next(
+          errorHandler(400, "Old password cannot be the same as old password")
+        );
+      }
+      if (newPassword.length < 6) {
+        return next(errorHandler(400, "Password must be over 6 characters"));
+      }
+      const validPassword: boolean = await compare(password, user.passwordHash);
+      if (!validPassword) {
+        return next(errorHandler(400, "Invalid password"));
+      } else {
+        const hashedPassword: string = await hash(newPassword, 10);
+        user.passwordHash = hashedPassword;
+      }
+    }
+
+    await user.save();
+
+    const userData: FormattedUser = formatUser(user);
+
+    res.status(200).json(userData);
   } catch (error) {
     next(error);
   }
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getUser, test } from "../controllers/user.controller";
+import { getUser, test, updateUser } from "../controllers/user.controller";
 import { protect } from "../middleware/protect";
 
 const router = Router();
 
 router.get("/test", test);
 router.get("/:identifier", protect, getUser);
+router.post("/update/", protect, updateUser);
 
 export default router;

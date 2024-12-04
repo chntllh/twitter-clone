f084ff15f77d8ef33c478ffaabbc5f25d340fbde
Siddhartha Singh <siddhartha.joy@gmail.com>
2024-12-04 13:27:55 +0530

    Add cors policy to backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import express, { NextFunction, Request, Response } from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config({ path: "../.env" });
 
@@ -13,6 +14,12 @@ mongoose
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.listen(3000, () => console.log("Server running on port 3000"));
 
